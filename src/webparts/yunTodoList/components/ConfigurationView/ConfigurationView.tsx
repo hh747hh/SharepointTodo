d95b8a651b9e34340272e119b974f38cc1e24044
Woo Yun <hh747hh@gmail.com>
2018-10-01 12:58:02 -0400

    first commit

---
+++ src/webparts/yunTodoList/components/ConfigurationView/ConfigurationView.tsx
@@ -0,0 +1,77 @@
+import * as React from "react";
+import { TextField, Button, ButtonType } from "office-ui-fabric-react";
+import styles from "./ConfigurationView.module.scss";
+
+export interface IConfigurationViewProps {
+  icon?: string;
+  iconText?: string;
+  description?: string;
+  buttonLabel?: string;
+  onConfigure?: () => void;
+}
+
+export interface IConfigurationViewState {}
+
+export default class ConfigurationView extends React.Component<
+  IConfigurationViewProps,
+  IConfigurationViewState
+> {
+  private _placeHolderText: string = "Enter your todo";
+
+  constructor(props: IConfigurationViewProps) {
+    super(props);
+
+    this.state = {
+      inputValue: ""
+    };
+
+    this._handleConfigureButtonClick = this._handleConfigureButtonClick.bind(
+      this
+    );
+  }
+
+  public render(): JSX.Element {
+    return (
+      <div className="Placeholder">
+        <div className="Placeholder-container ms-Grid">
+          <div className="Placeholder-head ms-Grid-row">
+            <div className="ms-Grid-col ms-u-hiddenSm ms-u-md3" />
+            <div className="Placeholder-headContainer ms-Grid-col ms-u-sm12 ms-u-md6">
+              <i
+                className={
+                  "Placeholder-icon ms-fontSize-su ms-Icon " + this.props.icon
+                }
+              />
+              <span className="Placeholder-text ms-fontWeight-light ms-fontSize-xxl">
+                {this.props.iconText}
+              </span>
+            </div>
+
+            <div className="ms-Grid-col ms-u-hiddenSm ms-u-md3" />
+          </div>
+          <div className="Placeholder-description ms-Grid-row">
+            <span className="Placeholder-descriptionText">
+              {this.props.description}
+            </span>
+          </div>
+          <div className="Placeholder-description ms-Grid-row">
+            <Button
+              className={styles.configureButton}
+              buttonType={ButtonType.primary}
+              ariaLabel={this.props.buttonLabel}
+              onClick={this._handleConfigureButtonClick}
+            >
+              {this.props.buttonLabel}
+            </Button>
+          </div>
+        </div>
+      </div>
+    );
+  }
+
+  private _handleConfigureButtonClick(
+    event?: React.MouseEvent<HTMLButtonElement>
+  ) {
+    this.props.onConfigure();
+  }
+}
---
+++ src/webparts/yunTodoList/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface IYunTodoListWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'YunTodoListWebPartStrings' {
+  const strings: IYunTodoListWebPartStrings;
+  export = strings;
+}
